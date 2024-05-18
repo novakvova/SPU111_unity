52f458743d7fd70928ea65eaa5896f4481105ac8
novakvova <novakvova@gmail.com>
2024-05-18 12:18:43 +0300

    Working game is good

---
+++ ApplePicker/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        basketList = new List<GameObject>();
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY+(basketSpacingY*i);
+            tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
+        }
+    }
+
+    public void AppleDestroyed()
+    {
+        GameObject[] list = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject go in list)
+        {
+            Destroy(go);
+        }
+        int index = basketList.Count - 1;
+        var item = basketList[index];
+        basketList.RemoveAt(index);
+        Destroy(item);
+    }
+}
---
+++ ApplePicker/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePrefab; //об'Їкт €блука, €кий буде падать з €блун≥
+    public float speed = 1f; //Ўвидк≥сть перем≥щенн€
+    public float leftAndRightEdge = 10f; //Ћ≥ва та права межа перем≥щенн€
+    public float chanceToChangeDirections = 0.1f; //≤мов≥рн≥сть зм≥ни напр€мку
+    public float secondsBetweenAppleDrops = 1f; //„астота скиданн€ €блук
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(DropApple), 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        //€блуко ставити на позиц≥ю €блун≥
+        apple.transform.position = this.transform.position;
+        Invoke(nameof(DropApple), secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if(pos.x<-leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if(pos.x>leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+}
